0917c9b6006739054036287e4e82d8a455e15b3f
lazytrainer09 <wihan0927@gmail.com>
2022-09-12 00:40:21 +0900

    section2

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,27 @@
 class Book < ApplicationRecord
+  enum sales_status: {
+    reservation: 0, # 予約受付
+    now_on_sale: 1, # 発売中
+    end_of_print: 2, # 販売終了
+  }
+
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_abount, -> (theme) { where("name like ?", "%#{theme}%") }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
 end
