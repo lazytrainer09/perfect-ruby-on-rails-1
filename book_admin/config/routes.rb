24c99723fe3a42f03293ee51aec25e0749b1b9fd
lazytrainer09 <wihan0927@gmail.com>
2022-09-13 23:21:33 +0900

    section3

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+
+  resources :publisher
+  resource :profile
 end
